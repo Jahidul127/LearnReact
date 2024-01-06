766dbabba11d1e28e5500268f6c67e124f56b931
jahidul127 <jahidul0248@gmail.com>
2024-01-06 22:05:12 +0600

    redux toolkit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -24,7 +22,8 @@ function App() {
 
     <h1>Counter Project Via React</h1>
     <h2>Counter Value: {counter}</h2>
-    <button onClick={addValue}>Add Value</button><br/>
+    <button 
+     onClick={addValue}>Add Value</button><br/>
     <button onClick={rmvValue}>Remove Value</button>
     </>
   )
